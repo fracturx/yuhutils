82628dd7004f712247c7a7ce36bcff08a88a8ef2
Fahim <fracturx@outlook.com>
2020-08-18 15:33:45 -0400

    improved textcomponents

--- src/main/kotlin/studio/yuhutils/servercommands/RequestCoords.kt
+++ src/main/kotlin/studio/yuhutils/servercommands/RequestCoords.kt
@@ -1,15 +1,16 @@
 package studio.yuhutils.servercommands
 
-import net.md_5.bungee.api.ChatColor
-import net.md_5.bungee.api.chat.ClickEvent
-import net.md_5.bungee.api.chat.ComponentBuilder
-import net.md_5.bungee.api.chat.HoverEvent
-import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabExecutor
 import org.bukkit.entity.Player
+import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.event.HoverEvent
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 
 
 class RequestCoords : TabExecutor {
@@ -18,12 +19,14 @@ class RequestCoords : TabExecutor {
             if (args.isNotEmpty()) {
                 val target = Bukkit.getServer().getPlayer(args[0])
                 if (target != null) {
-                    val message = TextComponent(ChatColor.DARK_PURPLE.toString() + sender.name + ChatColor.GREEN + " is requesting your coordinates. Would you like to share your coordinates with " + ChatColor.DARK_PURPLE + sender.name + "?")
-                    message.isUnderlined = true
-                    message.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/sharecoords " + sender.name)
-                    message.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                            ComponentBuilder("Are you sure you want to share your coordinates with this player?").color(ChatColor.DARK_RED).italic(true).create())
-                    target.spigot().sendMessage(message)
+                    val newMessage = TextComponent.builder().content(sender.name).color(NamedTextColor.DARK_PURPLE)
+                            .append(TextComponent.builder(" is requesting your coordinates. Would you like to share your coords with this player?").color(NamedTextColor.GREEN).build())
+                            .decoration(TextDecoration.UNDERLINED, true)
+                            .clickEvent(ClickEvent.of(ClickEvent.Action.RUN_COMMAND, "/sharecoords"))
+                            .hoverEvent(HoverEvent.showText(TextComponent.builder("Are you sure you want to share your coordinates with this player?").color(NamedTextColor.DARK_RED).build()))
+                            .build()
+                    val messageJSON = GsonComponentSerializer.gson().serialize(newMessage)
+                    target.sendRawMessage(messageJSON)
                 }
             } else {
                 sender.sendMessage("No player specified to send the data to.")
